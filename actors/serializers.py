43c66c13c4f86a7ff1781c3024de1ece3b74f417
Marcelo <marceloftex@gmail.com>
2025-12-30 16:36:00 -0300

    atualizando estrutura app actors

--- actors/serializers.py
+++ actors/serializers.py
@@ -5,3 +5,8 @@ class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = '__all__'
+
+class ActorRetriveUpdateDestroySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = '__all__'
--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
-from actors.views import ActorCreateListView
+from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,4 +10,5 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
 
     path("actors/", ActorCreateListView.as_view(), name='actor-create-list'),
+    path("actors/<int:pk>/", ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
 ]
