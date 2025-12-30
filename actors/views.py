1ca2bc14f2f0f9519a91ebf0ce015b0c38c85a64
Marcelo <marceloftex@gmail.com>
2025-12-30 15:53:25 -0300

    adicionando app actors

---
+++ actors/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics
+from actors.models import Actor
+from actors.serializers import ActorSerializer
+
+
+class ActorCreateListView(generics.ListCreateAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+    
--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
-
+from actors.views import ActorCreateListView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 
     path("genres/", GenreCreateListView.as_view(), name='genre-create-list'),
-    path("genres/<int:pk>/", GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view')
+    path("genres/<int:pk>/", GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
+
+    path("actors/", ActorCreateListView.as_view(), name='actor-create-list'),
 ]
