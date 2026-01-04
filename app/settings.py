33e6217b870efe8f22f8a169d9bf1761b45c3c23
Marcelo Teixeira <marceloftex@gmail.com>
2026-01-04 18:07:50 -0300

    Implementando camada de autenticação via JWT

--- app/settings.py
+++ app/settings.py
@@ -26,12 +26,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
 
+    "rest_framework",
+    "rest_framework_simplejwt",
+
+    "authentication",
     "genres",
     "actors",
     "movies",
     "reviews",
+    
 ]
 
 MIDDLEWARE = [
@@ -110,3 +114,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
