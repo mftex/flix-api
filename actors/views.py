e51038734801f3261fe802d175f43e8dfcc5bfb5
Marcelo Teixeira <marceloftex@gmail.com>
2026-01-04 18:41:24 -0300

    Incluindo autenticação nos endpoints

--- actors/views.py
+++ actors/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from actors.models import Actor
 from actors.serializers import ActorSerializer, ActorRetriveUpdateDestroySerializer
 
 
 class ActorCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
     
 class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Actor.objects.all()
     serializer_class = ActorRetriveUpdateDestroySerializer
--- genres/views.py
+++ genres/views.py
@@ -4,15 +4,18 @@ from django.shortcuts import get_object_or_404
 from genres.models import Genre
 import json
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from genres.serializers import GenreSerializer
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     
 
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
