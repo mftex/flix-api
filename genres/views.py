31d84df889a109d29376f3e4ec68e3b5c06ee4c1
Marcelo <marceloftex@gmail.com>
2026-01-06 08:23:16 -0300

    Pep8

--- genres/views.py
+++ genres/views.py
@@ -1,18 +1,14 @@
 from app.permissions import GlobalDefaultPermission
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
 from genres.models import Genre
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from genres.serializers import GenreSerializer
 
 
-
 class GenreCreateListView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    
 
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
--- movies/serializers.py
+++ movies/serializers.py
@@ -15,19 +15,20 @@ class MovieModelSerializer(serializers.ModelSerializer):
 
         if rate:
             return rate
-        
+
         return None
 
     def validate_release_date(self, value):
         if value.year < 1950:
             raise serializers.ValidationError('Ano de lançamento não pode ser anterior a 1950')
         return value
-    
+
     def validate_resume(self, value):
         if len(value) > 40:
             raise serializers.ValidationError(f'Tamanho do campo excede o limite de 40 caraceteres. Caracteres incluídos: {len(value)}')
         return value
-    
+
+
 class MovieStatsSerializer(serializers.Serializer):
     total_movies = serializers.IntegerField()
     movies_by_genre = serializers.ListField()
