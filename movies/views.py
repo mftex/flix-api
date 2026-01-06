20d8f64ff25d279119e9f6c2c22b7096de8ab6d8
Marcelo Teixeira <marceloftex@gmail.com>
2026-01-05 21:18:12 -0300

    Iniciando endpoint de estatísticas

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
 from app.permissions import GlobalDefaultPermission
 from django.shortcuts import render
-from rest_framework import generics 
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from movies.models import Movie
 from movies.serializers import MovieModelSerializer
@@ -15,3 +15,13 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
+    queryset = Movie.objects.all()
+
+    def get(self, request):
+        return response.Response(
+            data={'message': 'funcionou'},
+            status=status.HTTP_200_OK
+        )
